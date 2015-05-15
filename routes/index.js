8af06431f400301b0ec1783adea0f5600b9c051c
Peter Loftus <ploftus19@gmail.com>
2015-05-14 19:09:12 -0700

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Peter D Loftus' });
+});
+
+module.exports = router;
